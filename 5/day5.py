078f472d4bd1096fc633d4324cab09e9fafc845f
Varun Shenoy <vnshenoy@stanford.edu>
2020-12-05 11:22:09 -0800

    better solution for 5

--- 5/day5.py
+++ 5/day5.py
@@ -1,23 +1,24 @@
-def binaryFind(data, low, hi, lowerHalfChar="F", upperHalfChar="B"):
-    med = int((low + hi) / 2)
-    if low == hi:
-        return low
-    elif data[0] == lowerHalfChar:
-        return binaryFind(data[1:], low, med, lowerHalfChar=lowerHalfChar, upperHalfChar=upperHalfChar)
-    elif data[0] == upperHalfChar:
-        return binaryFind(data[1:], med + 1, hi, lowerHalfChar=lowerHalfChar, upperHalfChar=upperHalfChar)
+def binaryFind(data, lowerHalfChar="F", upperHalfChar="B"):
+    data = data.replace(lowerHalfChar, "0").replace(upperHalfChar, "1")
+    return int(data, 2)
+    # med = int((low + hi) / 2)
+    # if low == hi:
+    #     return low
+    # elif data[0] == lowerHalfChar:
+    #     return binaryFind(data[1:], low, med, lowerHalfChar=lowerHalfChar, upperHalfChar=upperHalfChar)
+    # elif data[0] == upperHalfChar:
+    #     return binaryFind(data[1:], med + 1, hi, lowerHalfChar=lowerHalfChar, upperHalfChar=upperHalfChar)
 
 
 def findSeatID(seat):
-    r = binaryFind(seat[:7], 0, 127)
-    c = binaryFind(seat[7:], 0, 7, lowerHalfChar="L", upperHalfChar="R")
+    r = binaryFind(seat[:7])
+    c = binaryFind(seat[7:], lowerHalfChar="L", upperHalfChar="R")
     return r * 8 + c
 
 
 def findMyID(seatIDs):
 
     # no point in doing binary search since the list is so small lol
-
     seatIDs.sort()
     for i in range(len(seatIDs) - 2):
         thisSeat = seatIDs[i]
